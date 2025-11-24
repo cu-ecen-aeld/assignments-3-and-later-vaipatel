bfee2f9ceb57ded3445ee7f9b0efa9795c4b4ba5
vaipatel <vaipatel@users.noreply.github.com>
2025-11-24 15:22:22 -0500

    Do assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,13 +14,34 @@
 */
 bool do_system(const char *cmd)
 {
+    if ( cmd == NULL )
+    {
+        return false;
+    }
+    
+    int ret;
+    ret = system(cmd);
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    // From man system:
+    // If a child process could not be created, or its status could
+    // not be retrieved, the return value is -1 and errno is set to
+    // indicate the error.
+    if ( ret == -1 )
+    {
+        perror("system() failed");
+        return false;
+    }
+
+    // From man system:
+    // If a shell could not be executed in the child process [.. or if ..]
+    // all system calls succeed [..] the return value is a "wait status" that
+    // can be examined using the macros described in waitpid(2).  (i.e.,
+    // WIFEXITED(), WEXITSTATUS(), and so on)
+    if ( !(WIFEXITED(ret) && WEXITSTATUS(ret) == 0) )
+    {
+        printf("A shell could not be executed in the child process, or the command failed\n");
+        return false;
+    }
 
     return true;
 }
@@ -45,19 +71,39 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    fflush(stdout);
+    pid_t pid;
+    pid = fork();
+    if ( pid == -1 )
+    {
+        perror("fork() failed");
+        return false;
+    }
+    else if ( pid == 0 )
+    {
+        // In the child: Replace image with command.
+        execv(command[0], command);
+
+        // If we got here, execv failed
+        perror("execv failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // In the parent: Wait for child to finish.
+    int status;
+    if ( waitpid(pid, &status, 0) == -1 )
+    {
+        perror("waitpid() failed");
+        return false;
+    }
+
+    // Check that the child exited normally
+    if ( !(WIFEXITED(status) && WEXITSTATUS(status) == 0) )
+    {
+        printf("The command failed\n");
+        return false;
+    }
 
     va_end(args);
 
@@ -80,18 +126,62 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
+    // Open a fd which will be duplicated onto stdout in the child
+    int fd;
+    fd = open(outputfile, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+    if ( fd == -1 )
+    {
+        perror("Could not open file for writing");
+        return false;
+    }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    pid_t pid;
+    pid = fork();
+    switch ( pid )
+    {
+        case -1:
+        {
+            perror("fork() failed");
+            return false;
+        }
+        case 0: // In the child
+        {
+            // Duplicate fd onto stdout
+            if (dup2(fd, STDOUT_FILENO) < 0)
+            {
+                perror("dup2");
+                return false;
+            }
+            close(fd);
+
+            // Replace image with command.
+            execv(command[0], command);
+
+            // If we got here, execv failed
+            perror("execv failed");
+            exit(EXIT_FAILURE);
+        }
+        default: // In the parent
+        {
+            close(fd);
+        }
+    }
+
+    // In the parent: Wait for child to finish.
+    int status;
+    if ( waitpid(pid, &status, 0) == -1 )
+    {
+        perror("waitpid() failed");
+        return false;
+    }
+
+    // Check that the child exited normally
+    if ( !(WIFEXITED(status) && WEXITSTATUS(status) == 0) )
+    {
+        printf("The command failed\n");
+        return false;
+    }
 
     va_end(args);
 
