80e75f9d97803f393691e2669c7898a6fe599e0a
vaipatel <vaipatel@users.noreply.github.com>
2025-11-30 05:37:33 -0500

    Do assignment 4 part 1

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,7 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
-
+    pthread_mutex_t* m_mutex;
+    int m_wait_to_obtain_ms;
+    int m_wait_to_release_ms;
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
